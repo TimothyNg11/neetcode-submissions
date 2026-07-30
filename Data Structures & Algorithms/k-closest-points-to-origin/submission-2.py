e796fbf552c25ace7d90a396726ecf0e3550bc40
TimothyNg11 <114866630+TimothyNg11@users.noreply.github.com>
2026-07-29 19:19:43 -0700

    Add: k-closest-points-to-origin - submission-2

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        arr = sorted(points, key=lambda x: math.sqrt(x[0] * x[0] + x[1] * x[1]))
+        return arr[:k]
+        
+
+
+        
