750e805018ce5a27dbdc0752bb99e03bb26aeae2
TimothyNg11 <114866630+TimothyNg11@users.noreply.github.com>
2026-08-09 02:03:16 -0700

    Add: word-ladder - submission-7

---
+++ Data Structures & Algorithms/word-ladder/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        self.arr = []
+        queue = deque()
+        queue.append((beginWord, 0, [beginWord]))
+
+        while queue:
+            curr, count, seen = queue.popleft()
+            if curr == endWord:
+                return count + 1
+            for word in wordList:
+                diff = len([1 for char1, char2 in zip(curr, word) if char1 != char2])
+                if diff == 1 and word not in seen:
+                    seen.append(word)
+                    queue.append((word, count + 1, seen))
+        
+        return 0
