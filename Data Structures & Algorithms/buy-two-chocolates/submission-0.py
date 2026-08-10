804ee19fe05a772258d90cb908644dee954f5757
TimothyNg11 <114866630+TimothyNg11@users.noreply.github.com>
2026-08-09 22:19:00 -0700

    Add: buy-two-chocolates - submission-0

---
+++ Data Structures & Algorithms/buy-two-chocolates/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        a = min(prices)
+        prices.remove(a)
+        b = min(prices)
+
+        if a + b <= money:
+            return money - (a + b)
+        return money
+        
